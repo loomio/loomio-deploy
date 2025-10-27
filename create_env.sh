a4d6152927e869cfdaecb1d93315f0560b7b6807
Robert Guthrie <rguthrie@gmail.com>
2025-10-27 15:43:49 +1300

    Update create_env.sh
    fix to use the renamed env_template file

--- create_env.sh
+++ create_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # strips all chars except . from hostname
 dots=${1//[^.]}
-sed "s/REPLACE_WITH_HOSTNAME/${1}/; s/REPLACE_WITH_CONTACT_EMAIL/${2}/" scripts/default_env > .env
+sed "s/REPLACE_WITH_HOSTNAME/${1}/; s/REPLACE_WITH_CONTACT_EMAIL/${2}/" env_template > .env
 echo DEVISE_SECRET=`openssl rand -base64 48` >> .env
 echo SECRET_COOKIE_TOKEN=`openssl rand -base64 48` >> .env
 echo RAILS_INBOUND_EMAIL_PASSWORD=`openssl rand -base64 48` >> .env
